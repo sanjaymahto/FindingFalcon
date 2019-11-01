e1db5f20fd8dec1f8373ac6cb93b6f25d9410e5a
sanjaymahto <mahto.sanjay41@gmail.com>
2019-11-02 02:53:44 +0530

    antd library added.

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Row, Col } from 'antd'
 import { bindActionCreators, Dispatch } from 'redux'
 import * as actions from './actions'
 import './index.scss'
 
 class Home extends React.PureComponent<any, any> {
   static propTypes: {
-    planets_metadata: PropTypes.Requireable<object>
-    vehicles_metadata: PropTypes.Requireable<object>
+    planets_metadata: PropTypes.Requireable<Array<any>>
+    vehicles_metadata: PropTypes.Requireable<Array<any>>
   }
 
   componentDidMount() {
@@ -17,13 +18,40 @@ class Home extends React.PureComponent<any, any> {
   }
 
   render() {
-    return <div className="HomeContainer"></div>
+    return (
+      <div className="HomeContainer">
+        <h1>Finding Falcone</h1>
+        <h4>Select planets you want to search in:</h4>
+        <Row type="flex" justify="space-around">
+          <Col span={4}>
+            <div>
+              <p>Destination 1</p>
+            </div>
+          </Col>
+          <Col span={4}>
+            <div>
+              <p>Destination 2</p>
+            </div>
+          </Col>
+          <Col span={4}>
+            <div>
+              <p>Destination 3</p>
+            </div>
+          </Col>
+          <Col span={4}>
+            <div>
+              <p>Destination 4</p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
 
 Home.propTypes = {
-  planets_metadata: PropTypes.object,
-  vehicles_metadata: PropTypes.object,
+  planets_metadata: PropTypes.array,
+  vehicles_metadata: PropTypes.array,
 }
 
 const mapStateToProps = (state: any) => {
