dab7efb8bc306564c9079ea00655c242514336b0
sanjaymahto <mahto.sanjay41@gmail.com>
2019-11-01 19:25:27 +0530

    prettier and ts linter added to the project.

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import configureStore from './store'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import configureStore from './RootStore'
 import Home from './Components/Home/index'
 import * as serviceWorker from './serviceWorker'
-import './index.css'
+import './index.scss'
 
 ReactDOM.render(
-    <Provider store={configureStore()}>
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                </Switch>
-            </div>
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={configureStore()}>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
